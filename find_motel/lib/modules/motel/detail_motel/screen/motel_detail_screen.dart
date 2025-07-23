6cc1ca570ef987c5d324b92e2489307d8ae24f49
CuongTV-GPS <tovucuong@gmail.com>
2025-07-24 01:56:54 +0700

    pull develop

--- find_motel/lib/modules/motel/detail_motel/screen/motel_detail_screen.dart
+++ find_motel/lib/modules/motel/detail_motel/screen/motel_detail_screen.dart
@@ -64,7 +64,9 @@ class _MotelDetailScreenState extends State<MotelDetailScreen> {
           return BlocConsumer<MotelDetailBloc, MotelDetailState>(
             listener: (context, state) {
               if (state is MotelDetailLoaded && state.needsReloadHome) {
-                blocContext.read<HomePageBloc>().add(LoadMotels());
+                blocContext.read<HomePageBloc>().add(
+                  LoadMotels(filter: AppDataManager().filterMotels, isRefresh: true)
+                  );
                 Navigator.pop(blocContext, true);
               } else if (state is MotelDetailError) {
                 ScaffoldMessenger.of(blocContext).showSnackBar(
